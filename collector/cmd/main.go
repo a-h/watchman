16057524c5751c91749cde8e3069ee76a5f26ca7
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-11-28 14:14:15 +0000

    Add support for getting comments on issues.

---
+++ collector/cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	"github.com/a-h/watchman/collector"
+)
+
+var token = flag.String("token", "", "GitHub auth token")
+var repo = flag.String("repo", "", "GitHub repo")
+
+func main() {
+	flag.Parse()
+	c := collector.NewCollector(*token)
+	issues, err := c.Issues(context.Background(), *repo)
+	if err != nil {
+		fmt.Printf("Error getting issue: %v\n", err)
+		return
+	}
+	for _, issue := range issues {
+		//TODO: Only get comments for issues that have been updated since the last time we checked.
+		comments, err := c.Comments(context.Background(), issue.Owner, issue.Repo, issue.Number)
+		if err != nil {
+			fmt.Printf("Error getting comments for issue: %v\n", err)
+			return
+		}
+		for _, c := range comments {
+			fmt.Printf("Comment: %s\n", c.URL)
+		}
+	}
+}
--- collector/watcher.go
+++ collector/watcher.go
@@ -16,6 +16,7 @@ import (
 // Collector uses the Github GraphQL API to collect data.
 type Collector struct {
 	githubToken string
+	Log         func(s string)
 }
 
 // maximumPageSize of Github GraphQL requests.
@@ -34,21 +35,31 @@ func (c *Collector) client(ctx context.Context) *graphql.Client {
 		&oauth2.Token{AccessToken: c.githubToken},
 	)
 	httpClient := oauth2.NewClient(ctx, src)
-	return graphql.NewClient("https://api.github.com/graphql",
+	client := graphql.NewClient("https://api.github.com/graphql",
 		graphql.WithHTTPClient(httpClient))
+	if c.Log != nil {
+		client.Log = c.Log
+	}
+	return client
 }
 
 // Issue within Github.
 type Issue struct {
+	Owner     string    `json:"owner"`
+	Repo      string    `json:"repo"`
 	URL       string    `json:"url"`
+	Number    int       `json:"number"`
 	BodyText  string    `json:"bodyText"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 // NewIssue creates an issue with all fields populated.
-func NewIssue(url, bodyText string, updatedAt time.Time) Issue {
+func NewIssue(owner, repo, url string, number int, bodyText string, updatedAt time.Time) Issue {
 	return Issue{
+		Owner:     owner,
+		Repo:      repo,
 		URL:       url,
+		Number:    number,
 		BodyText:  bodyText,
 		UpdatedAt: updatedAt,
 	}
@@ -70,7 +81,7 @@ func (c Collector) Issues(ctx context.Context, repository string) (issues []Issu
 			return
 		}
 		for _, n := range res.Repository.Issues.Nodes {
-			issues = append(issues, NewIssue(n.URL, n.BodyText, n.UpdatedAt))
+			issues = append(issues, NewIssue(owner, repo, n.URL, n.Number, n.BodyText, n.UpdatedAt))
 		}
 		cursor = &res.Repository.Issues.PageInfo.EndCursor
 		if !res.Repository.Issues.PageInfo.HasNextPage {
@@ -85,7 +96,7 @@ func (c Collector) issuesPage(ctx context.Context, owner, repo string, first int
 	req.Var("owner", owner)
 	req.Var("repo", repo)
 	req.Var("first", first)
-	req.Var("issueCursor", cursor)
+	req.Var("cursor", cursor)
 
 	req.Header.Set("Cache-Control", "no-cache")
 
@@ -93,15 +104,16 @@ func (c Collector) issuesPage(ctx context.Context, owner, repo string, first int
 	return
 }
 
-const issuesQuery = `query($owner:String!, $repo:String!, $first:Int!, $issueCursor:String) {
+const issuesQuery = `query($owner:String!, $repo:String!, $first:Int!, $cursor:String) {
   repository(owner: $owner, name: $repo) {
-    issues(first: $first, after: $issueCursor) {
+    issues(first: $first, after: $cursor) {
       pageInfo {
         endCursor
         hasNextPage
       }
       nodes {
-        url
+				url
+				number
         bodyText
         updatedAt
       }
@@ -118,10 +130,103 @@ type issuesQueryResult struct {
 			} `json:"pageInfo"`
 			Nodes []struct {
 				URL       string    `json:"url"`
+				Number    int       `json:"number"`
 				BodyText  string    `json:"bodyText"`
 				UpdatedAt time.Time `json:"updatedAt"`
 			} `json:"nodes"`
 		} `json:"issues"`
+	} `json:"repository"`
+}
+
+// A Comment on a Github issue.
+type Comment struct {
+	Owner       string    `json:"owner"`
+	Repo        string    `json:"repo"`
+	IssueNumber int       `json:"issueNumber"`
+	URL         string    `json:"url"`
+	BodyText    string    `json:"bodyText"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+// NewComment creates a comment with all required fields populated.
+func NewComment(owner, repo string, issueNumber int, url string, bodyText string, updatedAt time.Time) Comment {
+	return Comment{
+		Owner:       owner,
+		Repo:        repo,
+		IssueNumber: issueNumber,
+		URL:         url,
+		BodyText:    bodyText,
+		UpdatedAt:   updatedAt,
+	}
+}
+
+// Comments retrieves all of the comments for a particular issue.
+func (c Collector) Comments(ctx context.Context, owner, repo string, issueNumber int) (comments []Comment, err error) {
+	var cursor *string
+	for {
+		res, comErr := c.commentsPage(ctx, owner, repo, issueNumber, maximumPageSize, cursor)
+		if comErr != nil {
+			err = fmt.Errorf("collector: failed to get comments for repo '%s/%s/issues/%d': %v", owner, repo, issueNumber, comErr)
+			return
+		}
+		for _, n := range res.Repository.Issue.Comments.Nodes {
+			comments = append(comments, NewComment(owner, repo, issueNumber, n.URL, n.BodyText, n.UpdatedAt))
+		}
+		cursor = &res.Repository.Issue.Comments.PageInfo.EndCursor
+		if !res.Repository.Issue.Comments.PageInfo.HasNextPage {
+			return
+		}
+	}
+}
+
+func (c Collector) commentsPage(ctx context.Context, owner, repo string, issueNumber int, first int, cursor *string) (result commentsQueryResult, err error) {
+	req := graphql.NewRequest(commentsQuery)
+
+	req.Var("owner", owner)
+	req.Var("repo", repo)
+	req.Var("issueNumber", issueNumber)
+	req.Var("first", first)
+	req.Var("cursor", cursor)
+
+	req.Header.Set("Cache-Control", "no-cache")
+
+	err = c.client(ctx).Run(ctx, req, &result)
+	return
+}
+
+const commentsQuery = `query ($owner: String!, $repo: String!, $issueNumber: Int!, $first: Int!, $cursor: String) {
+  repository(owner: $owner, name: $repo) {
+    issue(number: $issueNumber) {
+      comments(first: $first, after: $cursor) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          url
+          updatedAt
+          bodyText
+        }
+      }
+    }
+  }
+}`
+
+type commentsQueryResult struct {
+	Repository struct {
+		Issue struct {
+			Comments struct {
+				PageInfo struct {
+					EndCursor   string `json:"endCursor"`
+					HasNextPage bool   `json:"hasNextPage"`
+				} `json:"pageInfo"`
+				Nodes []struct {
+					URL       string    `json:"url"`
+					UpdatedAt time.Time `json:"updatedAt"`
+					BodyText  string    `json:"bodyText"`
+				} `json:"nodes"`
+			} `json:"comments"`
+		} `json:"issue"`
 	} `json:"repository"`
 }
 
