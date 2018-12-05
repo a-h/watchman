c82295e5a1e12689f5d20d02b7f53b3828c17c25
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-12-05 07:53:02 +0000

    Added extra logging.

--- observer/repo/main.go
+++ observer/repo/main.go
@@ -7,9 +7,12 @@ import (
 
 	"github.com/a-h/watchman/observer/data"
 	"github.com/a-h/watchman/observer/github"
+	"github.com/a-h/watchman/observer/logger"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+const pkg = "github.com/a-h/watchman/observer/repo"
 
 type IssueLister func(ctx context.Context, repository string) (issues []github.Issue, err error)
 
@@ -36,17 +39,21 @@ func (h Handler) Handle(ctx context.Context, e events.SNSEvent) error {
 }
 
 func (h Handler) handle(ctx context.Context, repo data.Repository) error {
+	l := logger.For(pkg, "handle").WithField("repoUrl", repo.URL)
 	issues, err := h.ListIssues(ctx, repo.URL)
 	if err != nil {
+		l.WithError(err).Error("failed to list issues")
 		return fmt.Errorf("start: error listing issues for repo: %v", err)
 	}
 	for _, issue := range issues {
+		ll := l.WithField("issueUrl", issue.URL)
 		if issue.UpdatedAt.Before(repo.LastUpdated) {
-			//TODO: Log the behaviour?
+			ll.Info("skipping issue, not updated recently")
 			continue
 		}
 		err = h.SendToIssueInputQueue(issue)
 		if err != nil {
+			ll.WithError(err).Error("failed to send to Issue Input queue")
 			return fmt.Errorf("start: error sending to issue input queue for repo %v: %v", repo.URL, err)
 		}
 	}
--- observer/start/main.go
+++ observer/start/main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 
 	"github.com/a-h/watchman/observer/data"
+	"github.com/a-h/watchman/observer/logger"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+const pkg = "github.com/a-h/watchman/observer/start"
 
 // Handler handles incoming CloudWatch Timer elapsed events.
 type Handler struct {
@@ -17,17 +20,22 @@ type Handler struct {
 
 // Handle incoming CloudWatch Timer elapsed events.
 func (h Handler) Handle(ctx context.Context) error {
+	l := logger.For(pkg, "handle")
 	repos, err := h.ListRepositories()
 	if err != nil {
+		l.WithError(err).Error("failed to list repositories")
 		return fmt.Errorf("start: error listing repositories: %v", err)
 	}
 	for _, repo := range repos {
+		ll := l.WithField("repoUrl", repo.URL)
 		err = h.SendToRepoInputQueue(repo)
 		if err != nil {
+			ll.WithError(err).Error("failed to send to repo input queue")
 			return fmt.Errorf("start: error sending to issue input queue for repo %v: %v", repo.URL, err)
 		}
 		err = h.UpdateLastUpdatedDate(repo.URL)
 		if err != nil {
+			ll.WithError(err).Error("failed to set last updated date")
 			return fmt.Errorf("start: error updating last updated date for repo %v: %v", repo.URL, err)
 		}
 	}
